e00aeae526e03818aae088066167a1941724666c
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2018-12-27 10:20:05 +0530

    reactive form validations

--- angular/customermanager/src/app/reactiveform/reactiveform.component.ts
+++ angular/customermanager/src/app/reactiveform/reactiveform.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm, FormGroup, FormControl } from '@angular/forms';
-
+import { NgForm, FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
+import { test } from '../core/service';
+function ratingRange(c:AbstractControl):{[key:string]:boolean} | null{
+  console.log('rating validation')
+  if(c.value !== null && isNaN(c.value) || c.value < 1 || c.value > 5){
+    console.log('rating validation')
+    return {'range':true}
+  }
+  return null;
+}
 @Component({
   selector: 'app-reactiveform',
   templateUrl: './reactiveform.component.html',
@@ -9,14 +17,22 @@ import { NgForm, FormGroup, FormControl } from '@angular/forms';
 export class ReactiveformComponent implements OnInit {
 
   customersForm: FormGroup;
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
-    this.customersForm = new FormGroup({
-      firstName: new FormControl(''),
-      lastName: new FormControl(''),
-      email: new FormControl(''),
-      verify: new FormControl(true)
+    // this.customersForm = new FormGroup({
+    //   firstName: new FormControl('',[Validators.required,Validators.minLength(3)]),
+    //   lastName: new FormControl('',),
+    //   email: new FormControl(''),
+    //   verify: new FormControl(true)
+    // });
+    
+    this.customersForm = this.fb.group({
+      firstName: ['',[Validators.required,Validators.minLength(3)]],
+      lastName: ['',[Validators.required,Validators.minLength(3)]],
+      email: ['',[Validators.required,Validators.email]],
+      rating:['',ratingRange],
+      verify: 'true'
     });
   }
   save() {
