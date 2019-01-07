3796f26c81a7aa4c986bc45c65f6e6a537aaefa0
ngjsmdb <ngjsmdb@gmail.com>
2019-01-07 08:15:06 +0530

    feature module

--- angular/customermanager/src/app/reactiveform/reactiveform.component.ts
+++ angular/customermanager/src/app/reactiveform/reactiveform.component.ts
@@ -1,17 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { NgForm, FormGroup, FormControl, Validators, FormBuilder, AbstractControl, ValidatorFn } from '@angular/forms';
-import { test } from '../core/service';
-function ratingRange(min, max):ValidatorFn{
-  return  (c:AbstractControl):{[key:string]:boolean} | null => {
-    console.log('rating validation')
-    if(c.value !== null && isNaN(c.value) || c.value < min || c.value > max){
-      console.log('rating validation')
-      return {'range':true}
-    }
-    return null;
-  }
-=======
 import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl, ValidatorFn } from '@angular/forms';
 import { test } from '../core/service';
 
@@ -24,7 +11,6 @@ function ratingRange(min, max): ValidatorFn {
             }
             return null;
         };
->>>>>>> 7d5c5d4a7762859cbe3cfe9de8c8c049285844f0
 }
 
 function emailMatcher(c:AbstractControl):{[key:string]:boolean} | null{
@@ -45,14 +31,7 @@ function emailMatcher(c:AbstractControl):{[key:string]:boolean} | null{
 export class ReactiveformComponent implements OnInit {
   reactiveformComponentInfo:string;
   customersForm: FormGroup;
-<<<<<<< HEAD
-  constructor(private fb:FormBuilder) {
-    this.reactiveformComponentInfo = 'constructor of reactiveformComponentInfo';
-    console.log(this.reactiveformComponentInfo);
-  }
-=======
   constructor(private fb: FormBuilder) { }
->>>>>>> 7d5c5d4a7762859cbe3cfe9de8c8c049285844f0
 
   ngOnInit() {
     this.reactiveformComponentInfo = 'ngOninit of reactiveformComponentInfo';
@@ -66,23 +45,12 @@ export class ReactiveformComponent implements OnInit {
     // });
 
     this.customersForm = this.fb.group({
-<<<<<<< HEAD
-      firstName: ['',[Validators.required,Validators.minLength(3)]],
-      lastName: ['',[Validators.required,Validators.minLength(3)]],
-      emailGroup: this.fb.group({
-        email: ['',[Validators.required,Validators.email]],
-        confirmEmail:['',[Validators.required,Validators.email]],
-      },{validator:emailMatcher}),
-      
-      rating:['',ratingRange(1,5)],
-=======
       firstName: ['', [Validators.required, Validators.minLength(3)]],
       lastName: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       phone: '',
       sendNotification: 'email',
       rating: ['', ratingRange(1, 5)],
->>>>>>> 7d5c5d4a7762859cbe3cfe9de8c8c049285844f0
       verify: 'true'
     });
   }
