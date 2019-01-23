85e54cff678b8ab9d4399dd8521cb3cc25717b1f
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-01-23 10:31:54 +0530

    'ordersroutingmodule'

--- angular/managerapp/src/app/customer/customer.module.ts
+++ angular/managerapp/src/app/customer/customer.module.ts
@@ -2,12 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CustomerRoutingModule } from './customer-routing.module';
+import { CustomerComponent } from './customer.component';
+import { CustomerOrdersComponent } from './customer-orders.component';
+import { CustomerEditComponent } from './customer-edit.component';
+import { CustomerDetailsComponent } from './customer-details.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CustomerRoutingModule
   ],
-  declarations: []
+  declarations: [
+    CustomerComponent,
+    CustomerOrdersComponent,
+    CustomerEditComponent,
+    CustomerDetailsComponent
+  ]
 })
 export class CustomerModule { }
--- angular/managerapp/src/app/routingmodule.ts
+++ angular/managerapp/src/app/routingmodule.ts
@@ -1,26 +1,16 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CustomersComponent } from './customers/customerscomponent';
-import { OrdersComponent } from './orders/orders/orders.component';
-import { LoginComponent } from './login/login/login.component';
-
-import { CoreModule } from './core/core.module';
-import { CustomersModule } from './customers/customers.module';
-import { OrdersModule } from './orders/orders.module';
-import { LoginModule } from './login/login.module';
-import { CommonModule } from '@angular/common';
 
 const route: Routes = [
     {path: '' , redirectTo:'customers', pathMatch:'full'},
     {path: 'customers' , loadChildren: './customers/customers.module#CustomersModule'},
-    { path: 'order', component: OrdersComponent },
-    { path: 'login', component: LoginComponent },
-    {path: '**' , component: CustomersComponent}
+    { path: 'order', loadChildren: './orders/orders.module#OrdersModule' },
+    { path: 'login', loadChildren: './login/login.module#LoginModule' }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(route), CoreModule, CustomersModule, OrdersModule, LoginModule],
-    exports: [RouterModule, CoreModule, CustomersModule, OrdersModule, LoginModule]
+    imports: [RouterModule.forRoot(route)],
+    exports: [RouterModule]
 })
 
 export class RoutingModule {
